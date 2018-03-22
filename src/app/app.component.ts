ddb95b86e9ee9c91ca1d4acfc896faf042b0c48f
Sachin Singh <sachinx1000@gmail.com>
2018-03-22 22:56:32 +0530

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'deployment working fine!';
+  title = 'deployment working fine! again';
 }
