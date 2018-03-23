be95aa7995d5e97505fd69096f1efbab1acb77ac
Sachin Singh <sachinx1000@gmail.com>
2018-03-23 14:53:10 +0530

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+const appRoutes : Routes = [
+  {
+    path : "home",
+    component : HomeComponent
+  },
+  {
+    path : "about",
+    component : AboutComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
